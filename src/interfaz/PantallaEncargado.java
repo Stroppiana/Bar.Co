7f1a6ef855229131fc7ce9a3022844b8e38ed07f
Santiago <santti.72@hotmail.com.ar>
2023-11-01 16:06:47 -0300

    Cambios contenedor
    falta terminar

--- src/interfaz/PantallaEncargado.java
+++ src/interfaz/PantallaEncargado.java
@@ -6,113 +6,123 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 import logica.Cliente;
+import logica.Contenedor;
 import logica.Divisa;
 import logica.Encargado;
 import logica.InicioSesion;
+import logica.Producto;
 import logica.Puerto;
 import logica.RutaMaritima;
 
-public class PantallaEncargado implements InicioSesion{
-	
+public class PantallaEncargado implements InicioSesion {
+
 	public void MenuEncargado(Encargado encargado) {
-	    Cliente cli = new Cliente("", "", "", "");
-	    
+		Cliente cli = new Cliente("", "", "", "");
 
-	    String[] opEncargado = {"Asignar carga contenedor", "Generar RUTA MARITIMA", "Asignar Contenedor a barco", "Generar envíos", "Ver Envíos", "Eliminar clientes", "Mostrar clientes","Ingresar nueva divisa", "Salir"};
+		String[] opEncargado = { "Asignar carga contenedor", "Generar RUTA MARITIMA", "Asignar Contenedor a barco",
+				"Generar envíos", "Ver Envíos", "Eliminar clientes", "Mostrar clientes", "Ingresar nueva divisa",
+				"Salir" };
 
-	    int opciones = JOptionPane.showOptionDialog(null, "Ingrese la opción", null, 0, 0, null, opEncargado, opEncargado[0]);
+		int opciones = JOptionPane.showOptionDialog(null, "Ingrese la opción", null, 0, 0, null, opEncargado,
+				opEncargado[0]);
 
-	    switch (opciones) {
-	        case 0:
-	            JOptionPane.showMessageDialog(null, "Asignar CARGA-CONTENEDOR");
-	            break;
-	        case 1:
-	            JOptionPane.showMessageDialog(null, "Generar RUTA MARITIMA");
+		switch (opciones) {
+		case 0:
+			JOptionPane.showMessageDialog(null, "Asignar CARGA-CONTENEDOR");
 
-	            Puerto puerto = new Puerto();
-	  
-	            LinkedList<Puerto> puertos = puerto.mostrarPuertos();
+			Producto producto = null;
+			Contenedor contenedor = new Contenedor(opciones, opciones, false);
 
-	            
-                String[] opcionesPuerto = new String[puertos.size()];
+			try {
+				if (producto.getPeso() < contenedor.getCapacidad()) {
+					JOptionPane.showMessageDialog(null, "El producto podrá ser trasladado dentro de un contenedor");
 
+				} else if (producto.getFragil().equalsIgnoreCase("si")) {
+					JOptionPane.showMessageDialog(null, "El producto deber tener protección");
 
-	                for (int i = 0; i < puertos.size(); i++) {
-	                    opcionesPuerto[i] = puertos.get(i).getNombrePuerto();
-	                }
+				} else {
+					JOptionPane.showMessageDialog(null,
+							"El producto no entra en el contenedor. Debe reducir el peso del mismo");
+				}
 
-							String seleccion1 = (String) JOptionPane.showInputDialog(null, "Elija el PUERTO ORIGEN",
-									"Ventana", JOptionPane.DEFAULT_OPTION,
-									null, opcionesPuerto,
-									opcionesPuerto[0]);
-							String seleccion2 = (String) JOptionPane.showInputDialog(null, "Elija el PUERTO DESTINO",
-									"Ventana", JOptionPane.DEFAULT_OPTION,
-									null, opcionesPuerto,
-									opcionesPuerto[0]);
-							
-							System.out.println(seleccion1 +  " - " + seleccion2);
-							
-							int origen, destino;
-							
-							origen  = puerto.seleccionPuerto(seleccion1);
-							destino = puerto.seleccionPuerto(seleccion2);
-	
-							double distancia = 0;
-							
-						
-										
-							
-							
-							RutaMaritima rutaNueva = new RutaMaritima(origen, destino, distancia);
-							
-								if (rutaNueva.guardarDistanciaRuta(origen, destino)) {
-								
-									
-									distancia = rutaNueva.calcularDistancia(origen, origen, destino, destino);
-								}
-							
-							
-							
-							System.out.println("RUTA MARITIMA " + rutaNueva.generarRutaMaritima());
-							
-							//probando
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
 
-	            break;
-	        case 2:
-	            JOptionPane.showMessageDialog(null, "Asignar CONTENEDOR-BARCO");
-	            // Aquí puedes agregar el código para asignar contenedores a barcos.
-	            break;
-	        case 3:
-	            JOptionPane.showMessageDialog(null, "Generar envíos");
-	            // Aquí puedes agregar el código para generar envíos.
-	            break;
-	        case 4:
-	            JOptionPane.showMessageDialog(null, "Ver Envíos");
-	            // Aquí puedes agregar el código para ver los envíos.
-	            break;
-	        case 5:
-	            encargado.eliminarCliente(cli);
-	            break;
-	            
-	        case 6:
-	        	encargado.MostrarClientes();
-	        	break;
-	        case 7:
-	            String tipo = JOptionPane.showInputDialog("Ingrese tipo divisa");
-	            double coti = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la cotización"));
+			break;
+		case 1:
+			JOptionPane.showMessageDialog(null, "Generar RUTA MARITIMA");
 
-	            Divisa divisa = new Divisa(tipo, coti);
+			Puerto puerto = new Puerto();
 
-	            divisa.ingresarDivisa(divisa);
-	            break;
-	        case 8:
-	            JOptionPane.showMessageDialog(null, "Saliendo");
-	            break;
-	        default:
-	            JOptionPane.showMessageDialog(null, "Opción no válida", "Error", JOptionPane.ERROR_MESSAGE);
-	            break;
-	    }
+			LinkedList<Puerto> puertos = puerto.mostrarPuertos();
+
+			String[] opcionesPuerto = new String[puertos.size()];
+
+			for (int i = 0; i < puertos.size(); i++) {
+				opcionesPuerto[i] = puertos.get(i).getNombrePuerto();
+			}
+
+			String seleccion1 = (String) JOptionPane.showInputDialog(null, "Elija el PUERTO ORIGEN", "Ventana",
+					JOptionPane.DEFAULT_OPTION, null, opcionesPuerto, opcionesPuerto[0]);
+			String seleccion2 = (String) JOptionPane.showInputDialog(null, "Elija el PUERTO DESTINO", "Ventana",
+					JOptionPane.DEFAULT_OPTION, null, opcionesPuerto, opcionesPuerto[0]);
+
+			System.out.println(seleccion1 + " - " + seleccion2);
+
+			int origen, destino;
+
+			origen = puerto.seleccionPuerto(seleccion1);
+			destino = puerto.seleccionPuerto(seleccion2);
+
+			double distancia = 0;
+
+			RutaMaritima rutaNueva = new RutaMaritima(origen, destino, distancia);
+
+			if (rutaNueva.guardarDistanciaRuta(origen, destino)) {
+
+				distancia = rutaNueva.calcularDistancia(origen, origen, destino, destino);
+			}
+
+			System.out.println("RUTA MARITIMA " + rutaNueva.generarRutaMaritima());
+
+			// probando
+
+			break;
+		case 2:
+			JOptionPane.showMessageDialog(null, "Asignar CONTENEDOR-BARCO");
+			// Aquí puedes agregar el código para asignar contenedores a barcos.
+			break;
+		case 3:
+			JOptionPane.showMessageDialog(null, "Generar envíos");
+			// Aquí puedes agregar el código para generar envíos.
+			break;
+		case 4:
+			JOptionPane.showMessageDialog(null, "Ver Envíos");
+			// Aquí puedes agregar el código para ver los envíos.
+			break;
+		case 5:
+			encargado.eliminarCliente(cli);
+			break;
+
+		case 6:
+			encargado.MostrarClientes();
+			break;
+		case 7:
+			String tipo = JOptionPane.showInputDialog("Ingrese tipo divisa");
+			double coti = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la cotización"));
+
+			Divisa divisa = new Divisa(tipo, coti);
+
+			divisa.ingresarDivisa(divisa);
+			break;
+		case 8:
+			JOptionPane.showMessageDialog(null, "Saliendo");
+			break;
+		default:
+			JOptionPane.showMessageDialog(null, "Opción no válida", "Error", JOptionPane.ERROR_MESSAGE);
+			break;
+		}
 	}
-
 
 }
--- src/logica/Encargado.java
+++ src/logica/Encargado.java
@@ -1,4 +1,5 @@
 package logica;
+
 import javax.swing.JOptionPane;
 
 import datos.Conexion;
@@ -10,220 +11,193 @@ import java.sql.ResultSet;
 import java.util.LinkedList;
 import java.util.concurrent.atomic.AtomicInteger;
 
-
-public class Encargado extends Persona{
+public class Encargado extends Persona {
 	private int idEncargado;
 	private static final AtomicInteger indice = new AtomicInteger(0);
 
-	private LinkedList <Encargado> encargados = new LinkedList <>();
+	private LinkedList<Encargado> encargados = new LinkedList<>();
 
-	
-	
-		
 	public Encargado(String nombre, String apellido, String mail, String clave, String rol, int idEncargado) {
 		super(nombre, apellido, mail, clave);
 		this.idEncargado = idEncargado;
 		this.encargados = encargados;
 	}
 
-
-
 	public int getIdEncargado() {
 		return idEncargado;
 	}
-
-
-
 
 	public void setIdEncargado(int idEncargado) {
 		this.idEncargado = idEncargado;
 	}
 
-
 	public LinkedList<Encargado> getEncargados() {
 		return encargados;
 	}
-
-
 
 	public void setEncargados(LinkedList<Encargado> encargados) {
 		this.encargados = encargados;
 	}
 
-
-
-
 	public static AtomicInteger getIndice() {
 		return indice;
 	}
-
 
 	@Override
 	public String toString() {
 		return "Encargado [idEncargado=" + idEncargado + ", encargados=" + encargados + "]";
 	}
 
-
-	Conexion conexion = new Conexion ();
-	Connection con = conexion.conectar(); 
+	Conexion conexion = new Conexion();
+	Connection con = conexion.conectar();
 	PreparedStatement stmt;
-	
-	
-	/*
-	public void agregar () {
-		Encargado encargado3 = new Encargado("juan", "Perez", "ivanbilkis", "123456789", "Encargado", 1);
-		Encargado encargado2 = new Encargado("pedro", "Perez", "pedroperez", "123456789", "Encargado", 1);
-		Encargado encargado1 = new Encargado("Horacio", "Perez", "juanperez", "123456789", "Encargado", 1);
-		encargados.add(encargado1);
-		encargados.add(encargado2);
-		encargados.add(encargado3);
 
-		
-	}
-	*/
-	
-	public boolean inicioSesionEncargado () {
-		PantallaEncargado pe = new PantallaEncargado ();
-		
-		String sql= "SELECT * FROM `encargado` WHERE usuario=? AND clave=?";
-		
+	public boolean inicioSesionEncargado() {
+		PantallaEncargado pe = new PantallaEncargado();
+
+		String sql = "SELECT * FROM `encargado` WHERE usuario=? AND clave=?";
+
 		try {
-			   stmt = con.prepareStatement(sql);
-			    stmt.setString(1, this.getMail());
-			    stmt.setString(2, this.getClave());
-			    
-			    
-			    ResultSet resultados = stmt.executeQuery();
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, this.getMail());
+			stmt.setString(2, this.getClave());
 
+			ResultSet resultados = stmt.executeQuery();
 
-			    if (resultados.next()==true) {
-			    	pe.MenuEncargado(this);
+			if (resultados.next() == true) {
+				pe.MenuEncargado(this);
 
-			        return true;
-			        
-			    } else {
-			        return false;
-			    }
+				return true;
+
+			} else {
+				return false;
+			}
 		} catch (Exception e) {
-			 e.printStackTrace();
-			    JOptionPane.showMessageDialog(null, "Error en el inicio de sesión");
-			    return false;
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Error en el inicio de sesión");
+			return false;
 		}
-		
-		
+
 	}
-	public boolean eliminarCliente (Cliente cliente) {
+
+	public boolean eliminarCliente(Cliente cliente) {
 		String sql = "DELETE FROM `cliente` WHERE id_cliente=?";
 		int cli;
 		try {
-			
-			JOptionPane.showMessageDialog(null, "Va a eliminar un cliente a continuacion se le mostrara los clientes:\n"+this.MostrarClientes());
-			cli= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID para eliminarlo"));
+
+			JOptionPane.showMessageDialog(null,
+					"Va a eliminar un cliente a continuacion se le mostrara los clientes:\n" + this.MostrarClientes());
+			cli = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID para eliminarlo"));
 			cliente.setIdCliente(cli);
 
 			stmt = con.prepareStatement(sql);
-			
+
 			stmt.setInt(1, cliente.getIdCliente());
 			stmt.executeUpdate();
-			
+
 			JOptionPane.showMessageDialog(null, "Cliente exitosamente eliminado");
 			return true;
-			
+
 		} catch (Exception e) {
 			System.out.println("Error al guardar");
 			return false;
 		}
 
-	
 	}
+
 	public boolean sesionExistente(Cliente cliente) {
 		String sql = "SELECT * FROM `cliente` WHERE usuario=?";
 
 		try {
-			 stmt = con.prepareStatement(sql);
-			 stmt.setString(1, cliente.getMail());
-			
-			  ResultSet resultados = stmt.executeQuery();
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, cliente.getMail());
 
-			 
-			  if (resultados.next()) {
-				  JOptionPane.showMessageDialog(null, "Nombre de usuario ya existente");
+			ResultSet resultados = stmt.executeQuery();
+
+			if (resultados.next()) {
+				JOptionPane.showMessageDialog(null, "Nombre de usuario ya existente");
 				return true;
-				
-			}else {
+
+			} else {
 				return false;
 			}
 		} catch (Exception e) {
 			return false;
 		}
-		
-		
-		}
-	
-	public Cliente crearCliente (String nombre, String apellido, String mail, String clave) {
-		
-		
-		Cliente cliente = new Cliente (nombre, apellido, mail, clave);
-		
+
+	}
+
+	public Cliente crearCliente(String nombre, String apellido, String mail, String clave) {
+
+		Cliente cliente = new Cliente(nombre, apellido, mail, clave);
+
 		return cliente;
 	}
-	
+
 	public double calcularRutaMaritima(Barco barco) {
 		return 0;
 	}
-	
-	
-	Cliente cl = new Cliente ("", "", "", "");
-	
-	
-	public LinkedList <Cliente> MostrarClientes (){
-		
-		String sql="SELECT * from `cliente` WHERE 1";
-		String datos [] = new String [4];
-		
+
+	Cliente cl = new Cliente("", "", "", "");
+
+	public LinkedList<Cliente> MostrarClientes() {
+
+		String sql = "SELECT * from `cliente` WHERE 1";
+		String datos[] = new String[4];
+
 		try {
-			
+
 			stmt = con.prepareStatement(sql);
-			ResultSet resultados =	stmt.executeQuery();
+			ResultSet resultados = stmt.executeQuery();
 
 			while (resultados.next()) {
-				
-				
-				datos[0] = resultados.getString(1);
-				datos[1] =  resultados.getString(2);
-				datos [2] =   resultados.getString(3);
-				datos[3] = resultados.getString(4); 
-				
 
-				cl.getClientes().add(new Cliente (datos[0], datos[1], datos[2], datos[3]));
+				datos[0] = resultados.getString(1);
+				datos[1] = resultados.getString(2);
+				datos[2] = resultados.getString(3);
+				datos[3] = resultados.getString(4);
+
+				cl.getClientes().add(new Cliente(datos[0], datos[1], datos[2], datos[3]));
 			}
-			
+
 			if (cl.getClientes().isEmpty()) {
-				
-				
+
 				String mensaje = "No hay clientes";
 				System.out.println(mensaje);
 				return null;
-				
-			}else {
-				
+
+			} else {
+
 				return cl.getClientes();
 			}
 		} catch (Exception e) {
-			
+
 			System.out.println("Error");
 			return null;
-}
-		
-		
+		}
+
 	}
+
 	public void asignarContenedor(Producto producto, Contenedor contenedor) {
 		String sql = "SELECT * FROM `producto` WHERE peso=?";
-		
+
 		try {
-			
+			if (producto.getPeso() < contenedor.getCapacidad()) {
+				JOptionPane.showMessageDialog(null, "El producto podrá ser trasladado dentro de un contenedor");
+
+			} else if (producto.getFragil().equalsIgnoreCase("si")) {
+				JOptionPane.showMessageDialog(null, "El producto deber tener protección");
+
+			} else {
+				JOptionPane.showMessageDialog(null,
+						"El producto no entra en el contenedor. Debe reducir el peso del mismo");
+			}
+
 		} catch (Exception e) {
 			// TODO: handle exception
+
 		}
 	}
+	
+	
 }
